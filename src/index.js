e0ac635d2fba860ccafb0ea8829c5725f8f1a5cd
Paul Barrett <fastturtle4@gmail.com>
2018-12-19 22:26:17 -0500

    clicking on square adds X
    TODO: alternate from X to O and determine winner

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import "./styles.css";
-
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+  
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button className="square" 
+        onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i}/>;
   }
 
   render() {
-    const status = "Next player: X";
+    const status = 'Next player: X';
 
     return (
       <div>
@@ -58,4 +65,7 @@ class Game extends React.Component {
 
 // ========================================
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
