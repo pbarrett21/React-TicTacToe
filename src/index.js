cb78ceb3fcb8b63dfa5b45c794a88587cac14357
Paul Barrett <fastturtle4@gmail.com>
2018-12-20 00:01:31 -0500

    changed square class to function component

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-  
-  render() {
-    return (
-      <button className="square" 
-        onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+function Square(props) {
+  return (
+  <button 
+    className="square"
+    onClick={props.onClick}> 
+      {props.value}
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+  
+  handleClick(i) {
+    //create copy of squares array
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares})
+  }
+  
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square 
+             value={this.state.squares[i]}
+             onClick={() => this.handleClick(i)}
+             />;
   }
 
   render() {
